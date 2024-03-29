0662fcc0d4d8f665373ae00b03df45d8a7369af6
edmond <edmondmulera55@gmail.com>
2024-03-29 10:57:01 +0300

    models

--- dev_django/projects/models.py
+++ dev_django/projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=200, null=True, blank=True)
+    source_link = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = uuid
+
+
